9ea05c8719b1e92712b4785298f52387e53cee78
mattana57 <66010914057@msu.ac.th>
2026-01-28 16:05:14 +0700

    a

--- e/connectdb.php
+++ e/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
     $user = "root";
-    $pwd = "";
+    $pwd = "groupCar_toon05";
     $db = "4057db";
     $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมฐานข้อมูลไม่ได้");
     mysqli_query($conn,"set NAMES utf8");
