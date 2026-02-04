9ed07cfce45a3f202e6b68c498354be8596f80e2
mattana <66010914057@msu.ac.th>
2026-02-04 14:38:55 +0700

    a

--- h/a.php
+++ h/a.php
@@ -1,1 +1,25 @@
-<h1>มัทนา รัตนแสง(น้ำฝน)</h1>
+<?php
+    session_start
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง(น้ำฝน)</title>
+</head>
+
+<body>
+    <h1>a.php สร้าง session</h1>
+
+    <?php
+        $_SESSION['name'] = "มัทนา รัตนแสง"
+        $_SESSION['nickname'] = "น้ำฝน"
+        $_SESSION['p1'] = "โซฟา"
+        $_SESSION['p2'] = "ห่วงยาง"
+    ?>
+
+</body>
+</html>
+
+
+
---
+++ h/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง(น้ำฝน)</title>
+</head>
+
+<body>
+    <h1>เข้าสู่ระบบหลังบ้าน - มัทนา</h1>
+
+    <form method="post" action="">
+        Username <input type="text" name="auser" autofocus require><br>
+        Password <input type="password" name="apwd"  require><br>
+        <button type="submit" name="Submit">LOGIN</button>
+    </form>
+</body>
+</html>
