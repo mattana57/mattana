613677aca73b21e833d141a293a5953086b6a124
mattana <66010914057@msu.ac.th>
2026-02-14 16:02:03 +0700

    a

--- Project/connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-		$host = "localhost";
-		$user = "root";
-		$pwd = "groupCar_toon05";
-		$db = "goods_secret_store";
-		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
-		mysqli_query($conn, "SET NAMES utf8");
-?>
