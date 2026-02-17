e68639d045e384813dcc1895bd061eb58f558604
mattana57 <66010914057@msu.ac.th>
2026-02-17 19:57:59 +0700

    a

--- i/b.php
+++ i/b.php
@@ -26,6 +26,7 @@ if(isset($_POST['Submit'])){
     <tr>
         <th>รหัสจังหวัด</th>
         <th>ชื่อจังหวัด</th>
+        <th>รูป</th>
         <th>ลบ</th>
     </tr>
 <?php
@@ -37,6 +38,7 @@ while($data = mysqli_fetch_array($rs)){
     <tr>
         <td><?php echo $data['p_id']; ?></td>
         <td><?php echo $data['p_name']; ?></td>
+        <td><img src="images/<?php echo $data['p_id']; ?>.jpg" width="140"></td>
         <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.png" width="20"></a></td>
     </tr>
 <?php } ?>
