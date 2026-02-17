4b55299efff99923a2b2fb5fe2cbeafa3f54b9f5
mattana57 <66010914057@msu.ac.th>
2026-02-17 18:17:30 +0700

    a

--- i/a.php
+++ i/a.php
@@ -1,1 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง(น้ำฝน)</title>
+</head>
+
+<body>
 <h1>งานi --มัทนา รัตนแสง(น้ำฝน)</h1>
+
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM `regions`";
+$rs = mysqli_query($conn,$sql);
+while($data = mysqli_fetch_array($rs)){
+    echo $data['r_id']. "<br>";
+    echo $data['r_name']. "<hr>";
+}
+
+mysqli_close($conn);
+?>
+</body>
+</html>
+
