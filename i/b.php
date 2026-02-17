2c0668296db278ea8b229082868379a87da6ecf6
mattana57 <66010914057@msu.ac.th>
2026-02-17 20:02:45 +0700

    a

--- i/b.php
+++ i/b.php
@@ -9,7 +9,8 @@
 <h1>งานi --มัทนา รัตนแสง(น้ำฝน)</h1>
 
 <form method="post" action="">
-    ชื่อภาค <input type="text" name="rname" autofocus required>
+    ชื่อจังหวัด <input type="text" name="pname" autofocus required><br>
+    รูป <input type="file" name="pimage" required><br>
     <button type="submit" name="Submit"> บันทึก </button>
 </form><br><br>
 
