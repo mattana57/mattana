9c9ed8a237d7cd51d155a870eb44a95dc4fd9627
mattana <66010914057@msu.ac.th>
2026-02-17 16:26:50 +0700

    a

---
+++ project/add_to_cart.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'connectdb.php'; // ถ้ามีไฟล์เชื่อมต่อฐานข้อมูล
+
+if(!isset($_GET['id'])){
+    header("Location: index.php");
+    exit();
+}
+
+$id = $_GET['id'];
+
+/* สร้างตะกร้าใน session ถ้ายังไม่มี */
+if(!isset($_SESSION['cart'])){
+    $_SESSION['cart'] = [];
+}
+
+/* ถ้ามีสินค้าอยู่แล้ว เพิ่มจำนวน */
+if(isset($_SESSION['cart'][$id])){
+    $_SESSION['cart'][$id]++;
+}else{
+    $_SESSION['cart'][$id] = 1;
+}
+
+header("Location: cart.php");
+exit();
+?>
