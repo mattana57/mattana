194c822b3806c52b35f9e3e8732802984f1996df
mattana <66010914057@msu.ac.th>
2026-02-17 16:15:20 +0700

    a

--- project/includes/navbar.php
+++ project/includes/navbar.php
@@ -104,6 +104,19 @@ if(session_status() === PHP_SESSION_NONE){
                 </a>
 
             <?php } ?>
+            
+            <h4 class="section-title">ผลลัพธ์สินค้า</h4>
+<div class="row">
+<?php while($p = $products->fetch_assoc()){ ?>
+<div class="col-md-3 mb-4">
+<div class="card product-card p-3 text-center">
+<img src="images/<?= $p['image']; ?>" class="img-fluid mb-2">
+<h6><?= $p['name']; ?></h6>
+<p><?= number_format($p['price']); ?> บาท</p>
+<a href="product.php?id=<?= $p['id'] ?>" 
+   class="btn btn-light btn-sm mt-2">
+   รายละเอียด
+</a>
 
         </div>
 
