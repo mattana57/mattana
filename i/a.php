98fb614d2133785219e35a906b35e4aa6c169089
mattana57 <66010914057@msu.ac.th>
2026-02-17 18:57:58 +0700

    a

--- i/a.php
+++ i/a.php
@@ -8,15 +8,41 @@
 <body>
 <h1>งานi --มัทนา รัตนแสง(น้ำฝน)</h1>
 
+<form method="post" action="">
+    ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit"> บันทึก </button>
+</form><br><br>
+
+<?php
+if(isset($_POST['Submit'])){
+    include_once("connectdb.php");
+    $rname = $_POST['rname'];
+    $sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
+    mysqli_query($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
+}
+?>
+
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+        <th>ลบ</th>
+    </tr>
 <?php
 include_once("connectdb.php");
 $sql = "SELECT * FROM `regions`";
 $rs = mysqli_query($conn,$sql);
 while($data = mysqli_fetch_array($rs)){
-    echo $data['r_id']. "<br>";
-    echo $data['r_name']. "<hr>";
-}
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+        <td width="80" align="center"><img src="images/delete.png" width="20"></td>
+    </tr>
+<?php } ?>
+</table>
 
+<?php
 mysqli_close($conn);
 ?>
 </body>
