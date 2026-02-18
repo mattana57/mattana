3c2768f102447e80fa5278017baa9f08aea2c6dc
mattana57 <66010914057@msu.ac.th>
2026-02-18 16:54:13 +0700

    ฟ

---
+++ k/a.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง (น้ำฝน)</title>
+</head>
+
+<body>
+    <h1>งาน k 66010914057 มัทนา รัตนแสง (น้ำฝน)</h1>
+    <!--<button type="button" class="btn btn-success">มัทนา</button>
+    <button type="button" class="btn btn-warning">Warning</button> 
+    <img src="images/1.jpg" width ="240"> -->
+
+    <button type="button" class="btn btn-success" onclick="showImage('images/1.jpg')">คลิก</button>
+    <button type="button" class="btn btn-warning" onclick="showImage('images/2.jpg')">คลิก</button>
+</div>
+</body>
+</html>
