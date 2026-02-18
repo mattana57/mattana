2936db9eca363fb6357d1958ed566bb6a4ae033f
mattana57 <66010914057@msu.ac.th>
2026-02-18 16:58:03 +0700

    ฟ

--- k/a.php
+++ k/a.php
@@ -1,18 +1,18 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>66010914057 มัทนา รัตนแสง (น้ำฝน)</title>
+    <meta charset="utf-8">
+    <title>66010914057 มัทนา รัตนแสง (น้ำฝน)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
-<body>
+<body class="container mt-4">
     <h1>งาน k 66010914057 มัทนา รัตนแสง (น้ำฝน)</h1>
-    <!--<button type="button" class="btn btn-success">มัทนา</button>
-    <button type="button" class="btn btn-warning">Warning</button> 
-    <img src="images/1.jpg" width ="240"> -->
-    <div class="container mt-4">
-        <button type="button" class="btn btn-success" onclick="showImage('images/1.jpg')">คลิก</button>
-        <button type="button" class="btn btn-warning" onclick="showImage('images/2.jpg')">คลิก</button>
+    
+    <button class="btn btn-success" onclick="pic.src='images/1.jpg'">คลิก </button>
+    <button class="btn btn-warning" onclick="pic.src='images/2.jpg'">คลิก </button>
+    
+    <div class="mt-3">
+        <img id="pic" src="images/1.jpg" width="240" class="img-thumbnail">
     </div>
 </body>
 </html>
