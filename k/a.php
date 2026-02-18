f07724950bdf10d465def35de0ae680c2790beac
mattana57 <66010914057@msu.ac.th>
2026-02-18 17:02:33 +0700

    a

--- k/a.php
+++ k/a.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="utf-8">
     <title>66010914057 มัทนา รัตนแสง (น้ำฝน)</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-4">
     <h1>งาน k 66010914057 มัทนา รัตนแสง (น้ำฝน)</h1>
     
-    <button class="btn btn-success" onclick="pic.src='images/1.jpg'; pic.style.display='block'">คลิก (เขียว)</button>
-    <button class="btn btn-warning" onclick="pic.src='images/2.jpg'; pic.style.display='block'">คลิก (ส้ม)</button>
+    <button class="btn btn-success" onclick="pic.src='images/1.jpg'; pic.style.display='block'">คลิก</button>
+    <button class="btn btn-warning" onclick="pic.src='images/2.jpg'; pic.style.display='block'">คลิก</button>
     
     <div class="mt-3">
         <img id="pic" src="" width="240" class="img-thumbnail" style="display:none">
