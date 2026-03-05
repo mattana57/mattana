5db56fea5ebfd2962bd5028c5dcb6464e389fade
mattana57 <66010914057@msu.ac.th>
2026-03-05 12:21:37 +0700

    t

--- k/a.php
+++ k/a.php
@@ -4,6 +4,22 @@
     <meta charset="utf-8">
     <title>66010914057 มัทนา รัตนแสง (น้ำฝน)</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    .btn-student {
+        background-color: #28a745;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        cursor: pointer;
+    }
+    .btn-teacher {
+        background-color: #ffc107;
+        color: black;
+        padding: 10px 20px;
+        border: none;
+        cursor: pointer;
+    }
+</style>
 </head>
 <body class="container mt-4">
     <h1>งาน k 66010914057 มัทนา รัตนแสง (น้ำฝน)</h1>
@@ -15,8 +31,6 @@
     <button type="button" class="btn-teacher" onclick="window.location.href='images/2.jpg'">
         อาจารย์
     </button>
-    <div class="mt-3">
-        <img id="pic" src="" width="240" class="img-thumbnail" style="display:none">
-    </div>
+    
 </body>
 </html>
