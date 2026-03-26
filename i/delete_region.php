90690d9c7b4858efcd289773e6135df03a3ec40b
mattana <66010914057@msu.ac.th>
2026-03-26 12:35:56 +0700

    ส

--- i/delete_region.php
+++ i/delete_region.php
@@ -3,8 +3,11 @@
 include_once("connectdb.php");
 
 $id= $_GET['id'];
+$ext = $_GET['ext'];
 $sql = "DELETE FROM regions WHERE r_id ='{$id}'";
 mysqli_query($conn,$sql) or die ("ลบข้อมูลไม่ได้");
+
+unlink("images/".$id.".".$ext);
 
 echo"<script>";
 echo"window.location='a.php'";
